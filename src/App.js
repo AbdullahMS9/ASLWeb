cd435af5a83405e030b5f92ffd5126c4731a740c
AbdullahMS9 <65154836+AbdullahMS9@users.noreply.github.com>
2022-09-25 06:49:18 -0400

    Base Web

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id="splash-screen">
+            <div id="splash-welcome"><h6>Welcome to the... <br/> ASL Translator</h6></div>
+            <div id="splash-message"> Helping to close the gap between the deaf and hearing community 
+            <br/>
+            Interpreter:<br/>
+            </div>
+        <div id="splash-student"><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <strong>Site developed by some Hackers ©</strong></div>
+    </div>
+  );
+}
+
+export default App;
